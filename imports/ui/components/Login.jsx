ff42f5f2afb8ac708169ccb3c85e7cd67edb7439
aman-atg <amanansari.a2@gmail.com>
2020-03-06 20:30:42 +0530

    login functionality

--- imports/ui/components/Login.jsx
+++ imports/ui/components/Login.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { Link } from "react-router";
+import { Meteor } from "meteor/meteor";
+class Login extends React.Component {
+  state = {
+    error: ""
+  };
 
-const Login = () => {
-  return <h1>Login</h1>;
-};
+  onFormSubmit = e => {
+    e.preventDefault();
+    let email = e.target.email.value;
+    let password = e.target.password.value;
+
+    Meteor.loginWithPassword({ email }, password, err => {
+      console.log("Login callback ", err);
+      if (err !== undefined) {
+        this.setState({ error: err.reason });
+      }
+    });
+  };
+  render() {
+    let { error } = this.state;
+    return (
+      <div>
+        {error ? <p>{error}</p> : undefined}
+
+        <form onSubmit={this.onFormSubmit}>
+          <input type="email" name="email" placeholder="abc@example.com" />
+          <input
+            type="password"
+            name="password"
+            placeholder="myStrongPassword"
+          />
+          <button>Login</button>
+        </form>
+
+        <Link to="/signup">Create an account.</Link>
+      </div>
+    );
+  }
+}
 
 export default Login;
