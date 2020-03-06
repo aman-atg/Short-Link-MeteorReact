1e1e4598434e3e91edba96cb3d36733f4e6fb9f3
aman-atg <amanansari.a2@gmail.com>
2020-03-06 17:12:25 +0530

    add form in SignUp

--- imports/ui/components/SignUp.jsx
+++ imports/ui/components/SignUp.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Link } from "react-router";
+class SignUp extends React.Component {
+  state = {};
+  onFormSubmit = e => {
+    e.preventDefault();
+  };
+  render() {
+    return (
+      <div>
+        <h1>Join short Lnk</h1>
+        <form onSubmit={this.onFormSubmit}>
+          <input type="email" name="Email" id="" />
+          <input type="password" name="Password" id="" />
+          <button>Create Account</button>
+        </form>
 
-const SignUp = () => {
-  return <h1>SignUp</h1>;
-};
+        <Link to="/">Already have an account</Link>
+      </div>
+    );
+  }
+}
 
 export default SignUp;
