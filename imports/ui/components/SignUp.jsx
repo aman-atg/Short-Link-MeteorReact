7dae96adec004f1001996e68fc55f58392875f15
aman-atg <amanansari.a2@gmail.com>
2020-03-06 17:54:13 +0530

    create user in mongo

--- imports/ui/components/SignUp.jsx
+++ imports/ui/components/SignUp.jsx
@@ -1,17 +1,35 @@
 import React from "react";
 import { Link } from "react-router";
+import { Accounts } from "meteor/accounts-base";
+
 class SignUp extends React.Component {
-  state = {};
+  state = {
+    error: ""
+  };
   onFormSubmit = e => {
     e.preventDefault();
+    let email = e.target.email.value;
+    let password = e.target.password.value;
+
+    Accounts.createUser({ email, password }, err => {
+      console.log("Signup callback ", err);
+    });
+
+    // this.setState({ error: "" });
   };
   render() {
+    let { error } = this.state;
     return (
       <div>
+        {error ? <p>{error}</p> : undefined}
         <h1>Join short Lnk</h1>
         <form onSubmit={this.onFormSubmit}>
-          <input type="email" name="Email" id="" />
-          <input type="password" name="Password" id="" />
+          <input type="email" name="email" placeholder="abc@example.com" />
+          <input
+            type="password"
+            name="password"
+            placeholder="myStrongPassword"
+          />
           <button>Create Account</button>
         </form>
 
