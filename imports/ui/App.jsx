a7f7d376f96c9889cb2b76cbd22ef04a6425c4bc
aman-atg <amanansari.a2@gmail.com>
2020-03-07 00:06:52 +0530

    some modifications

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Route, Router, browserHistory } from "react-router";
+import { Route, Router, Redirect, browserHistory } from "react-router";
+import { Meteor } from "meteor/meteor";
 
 //
 import Login from "./components/Login";
@@ -8,11 +9,17 @@ import Link from "./components/Link";
 import NotFound from "./components/NotFound";
 
 const App = () => {
+  const onEnterPublicPage = () => {
+    if (!!Meteor.userId()) <Redirect to="/links" />;
+  };
+  const onEnterPrivatePage = () => {
+    if (!Meteor.userId()) <Redirect to="/" />;
+  };
   const routes = (
     <Router history={browserHistory}>
-      <Route path="/" component={Login} />
-      <Route path="/signup" component={SignUp} />
-      <Route path="/links" component={Link} />
+      <Route path="/" component={Login} onEnter={onEnterPublicPage} />
+      <Route path="/signup" component={SignUp} onEnter={onEnterPublicPage} />
+      <Route path="/links" component={Link} onEnter={onEnterPrivatePage} />
       <Route path="*" component={NotFound} />
     </Router>
   );
