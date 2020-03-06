eb78a64ff89a69a6b753c89721c4614d7de5e4e3
aman-atg <amanansari.a2@gmail.com>
2020-03-06 17:11:44 +0530

    added more routes

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import { Route, Router, browserHistory } from "react-router";
 
 //
+import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import Link from "./components/Link";
+import NotFound from "./components/NotFound";
 
 const App = () => {
   const routes = (
     <Router history={browserHistory}>
-      <Route />
+      <Route path="/" component={Login} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/links" component={Link} />
+      <Route path="*" component={NotFound} />
     </Router>
   );
-  return <Link />;
+  return routes;
 };
 export default App;
