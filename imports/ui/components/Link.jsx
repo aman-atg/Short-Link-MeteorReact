42651e21d2cb5518b41e3df7bf8d9664d559a0c3
aman-atg <amanansari.a2@gmail.com>
2020-03-07 19:26:03 +0530

    link component

--- imports/ui/components/Link.jsx
+++ imports/ui/components/Link.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import { Accounts } from "meteor/accounts-base";
+import { Links } from "../../apis/links";
+import LinkList from "./LinkList";
 const Link = () => {
   const onLogout = () => {
     Accounts.logout();
   };
+  const onFormSubmit = e => {
+    e.preventDefault();
+    const url = e.target.url.value.trim();
+    Links.insert({ url }, err => {
+      console.log("links form : ", err);
+    });
+  };
   return (
     <div>
-      <h1>your links </h1>
+      <h1>Your links </h1>
+      <LinkList />
       <button onClick={onLogout}> Logout </button>
+      <hr />
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="url" />
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
