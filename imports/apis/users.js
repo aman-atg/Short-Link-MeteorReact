8c070dbc5e266f2e27311f6f9bd1006f0676a7ed
aman-atg <amanansari.a2@gmail.com>
2020-03-08 00:42:38 +0530

    refactor

--- imports/apis/users.js
+++ imports/apis/users.js
@@ -3,16 +3,13 @@ import { Accounts } from "meteor/accounts-base";
 import SimpleSchema from "simpl-schema";
 
 Accounts.validateNewUser(user => {
-  try {
-    const email = user.emails[0].address;
-    const UserSchema = new SimpleSchema({
-      email: {
-        type: String,
-        regEx: SimpleSchema.RegEx.Email
-      }
-    }).validate({ email });
-    return true;
-  } catch (error) {
-    throw new Meteor.Error(400, error.message); //without Meteor.Error... the error will be shown only on server
-  }
+  const email = user.emails[0].address;
+  const UserSchema = new SimpleSchema({
+    email: {
+      type: String,
+      regEx: SimpleSchema.RegEx.Email
+    }
+  }).validate({ email });
+
+  return true;
 });
