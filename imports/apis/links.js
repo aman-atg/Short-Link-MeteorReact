bd17495009d36e5c55c14291d80b9f94e9cf952e
aman-atg <amanansari.a2@gmail.com>
2020-03-08 00:42:07 +0530

    link insert&validate method

--- imports/apis/links.js
+++ imports/apis/links.js
@@ -1,8 +1,24 @@
 import { Mongo } from "meteor/mongo";
 import { Meteor } from "meteor/meteor";
+import { UrlSchema } from "../apis/url";
 export const Links = new Mongo.Collection("links");
 if (Meteor.isServer) {
   Meteor.publish("links", () => {
     return Links.find({ userId: Meteor.userId() });
   });
 }
+
+Meteor.methods({
+  "links.insert"(url) {
+    const userId = Meteor.userId();
+    if (!userId)
+      throw new Meteor.Error(
+        "unauthorize",
+        "You don't have permission to do that"
+      );
+    UrlSchema.validate({ url });
+    Links.insert({ url, userId }, err => {
+      console.log("links form : ", err);
+    });
+  }
+});
