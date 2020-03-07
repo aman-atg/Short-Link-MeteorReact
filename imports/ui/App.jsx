840b1552c344cc089529ed71626a5157c9cf75cb
aman-atg <amanansari.a2@gmail.com>
2020-03-07 16:58:17 +0530

    refactor

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import { Route, Router, Redirect, browserHistory } from "react-router";
 import { Meteor } from "meteor/meteor";
-
 //
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import Link from "./components/Link";
 import NotFound from "./components/NotFound";
 
+const unauthPages = ["/", "/signup"],
+  authPages = ["/links"];
+
+export const onAuthChange = isAuthenticated => {
+  const { pathname } = browserHistory.getCurrentLocation();
+  const isAuthPage = authPages.includes(pathname);
+  const isUnauthPage = unauthPages.includes(pathname);
+
+  //if logged in && on unauthPages then redirect to links
+  if (isAuthenticated && isUnauthPage) browserHistory.replace("/links");
+  //if not logged in && on authpage then redirect to login
+  else if (!isAuthenticated && isAuthPage) browserHistory.replace("/");
+};
+
 const App = () => {
   const onEnterPublicPage = () => {
-    if (!!Meteor.userId()) <Redirect to="/links" />;
+    if (Meteor.userId()) <Redirect to="/links" />;
   };
   const onEnterPrivatePage = () => {
     if (!Meteor.userId()) <Redirect to="/" />;
