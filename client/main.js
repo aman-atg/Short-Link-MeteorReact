4d0c3d8a3c8b01623c2bb975fe2cf1c87949d8bb
aman-atg <amanansari.a2@gmail.com>
2020-03-07 16:57:28 +0530

    refactor main.js

--- client/main.js
+++ client/main.js
@@ -1,25 +1,15 @@
 import { Meteor } from "meteor/meteor";
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import { Tracker } from "meteor/tracker";
 //
-import App from "../imports/ui/App";
-import { browserHistory } from "react-router";
-const unauthPages = ["/", "/signup"],
-  authPages = ["/links"];
+import App, { onAuthChange } from "../imports/ui/App";
 
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId();
+  onAuthChange(isAuthenticated);
   //!! DEBUG
   //console.log("isAuthenticated :", isAuthenticated);
-  const { pathname } = browserHistory.getCurrentLocation();
-  const isAuthPage = authPages.includes(pathname);
-  const isUnauthPage = unauthPages.includes(pathname);
-
-  //if logged in && on unauthPages then redirect to links
-  if (isAuthenticated && isUnauthPage) browserHistory.push("/links");
-  //if not logged in && on authpage then redirect to login
-  else if (!isAuthenticated && isAuthPage) browserHistory.push("/");
 });
 
 Meteor.startup(() => {
