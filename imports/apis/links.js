b844e97312a2f939b97c7f348d465167e4fac77b
aman-atg <amanansari.a2@gmail.com>
2020-03-07 20:49:42 +0530

    publish links

--- imports/apis/links.js
+++ imports/apis/links.js
@@ -1,2 +1,8 @@
 import { Mongo } from "meteor/mongo";
+import { Meteor } from "meteor/meteor";
 export const Links = new Mongo.Collection("links");
+if (Meteor.isServer) {
+  Meteor.publish("links", () => {
+    return Links.find({ userId: Meteor.userId() });
+  });
+}
