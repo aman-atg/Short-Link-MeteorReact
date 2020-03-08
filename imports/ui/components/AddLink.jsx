6cb2bdb9a74df9952bff6c3781331e58007ae2ba
aman-atg <amanansari.a2@gmail.com>
2020-03-09 00:43:36 +0530

    control the url input

--- imports/ui/components/AddLink.jsx
+++ imports/ui/components/AddLink.jsx
@@ -1,20 +1,34 @@
 import React from "react";
-// import { Meteor } from "meteor/meteor";
-const AddLink = () => {
-  const onFormSubmit = e => {
-    e.preventDefault();
-    const url = e.target.url.value.trim();
-    Meteor.call("links.insert", url);
-    e.target.url.value = "";
-    console.log(Meteor);
-  };
+import { Meteor } from "meteor/meteor";
 
-  return (
-    <form onSubmit={onFormSubmit}>
-      <input type="text" name="url" />
-      <button>Submit</button>
-    </form>
-  );
-};
+class AddLink extends React.Component {
+  state = {
+    url: ""
+  };
+  onUrlChange = e => {
+    const url = e.target.url.value;
+    this.setState({ url });
+  };
+  render() {
+    const onFormSubmit = e => {
+      e.preventDefault();
+      const { url } = this.state;
+      Meteor.call("links.insert", url.trim());
+      this.setState({ url: "" });
+    };
+
+    return (
+      <form onSubmit={onFormSubmit}>
+        <input
+          type="text"
+          name="url"
+          onChange={this.onUrlChange}
+          value={this.state.url}
+        />
+        <button>Submit</button>
+      </form>
+    );
+  }
+}
 
 export default AddLink;
