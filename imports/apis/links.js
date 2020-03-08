cc8cc265ea8c41a387132cd3f715c2a8cf11c387
aman-atg <amanansari.a2@gmail.com>
2020-03-08 20:17:43 +0530

    add validation and required methods

--- imports/apis/links.js
+++ imports/apis/links.js
@@ -2,10 +2,11 @@ import { Mongo } from "meteor/mongo";
 import { Meteor } from "meteor/meteor";
 import { UrlSchema } from "../apis/url";
 import shortId from "shortid";
+import SimpleSchema from "simpl-schema";
 export const Links = new Mongo.Collection("links");
 if (Meteor.isServer) {
   Meteor.publish("links", () => {
-    return Links.find({ userId: Meteor.userId() });
+    return Links.find({ userId: Meteor.userId(), visible: true });
   });
 }
 
@@ -20,8 +21,28 @@ Meteor.methods({
       );
     UrlSchema.validate({ url });
 
-    Links.insert({ _id, url, userId }, err => {
+    Links.insert({ _id, url, userId, visible: true }, err => {
       console.log("links form : ", err);
     });
+  },
+  "link.setVisibility"(_id, visible) {
+    //validation
+    if (!Meteor.userId())
+      throw new Meteor.Error(
+        "unauthorize",
+        "You don't have permission to do that"
+      );
+    new SimpleSchema({
+      _id: { type: String, min: 4 },
+      visible: { type: Boolean }
+    }).validate({ _id, visible });
+    //main task
+    Links.update(
+      { _id, userId: Meteor.userId() },
+      { $set: { visible: !visible } }
+    );
   }
+  // 'links.showHidden'(userId,flg){
+  //   Links.update({userId})
+  // }
 });
