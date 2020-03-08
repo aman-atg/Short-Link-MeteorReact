49918fc184297732731a4a89a1c262944bc5dba0
aman-atg <amanansari.a2@gmail.com>
2020-03-08 12:23:15 +0530

    redirect custom links

--- server/main.js
+++ server/main.js
@@ -1,6 +1,19 @@
 import { Meteor } from "meteor/meteor";
+import { WebApp } from "meteor/webapp";
 import "../imports/apis/users";
 import "../imports/apis/links";
 import "../startup/simpl-schema-config";
+import { Links } from "../imports/apis/links";
 
-Meteor.startup(() => {});
+Meteor.startup(() => {
+  WebApp.connectHandlers.use((req, res, next) => {
+    const _id = req.url.slice(1);
+    const ourLink = Links.findOne({ _id });
+    if (ourLink) {
+      res.statusCode = 302;
+      res.setHeader("Location", ourLink.url);
+      res.end();
+    }
+    next();
+  });
+});
