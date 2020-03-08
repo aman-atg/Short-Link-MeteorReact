dbf7170257f8fb65d20d1c78f40aabe74d81c4e3
aman-atg <amanansari.a2@gmail.com>
2020-03-09 00:32:28 +0530

    new Toggler component

--- imports/ui/components/Link.jsx
+++ imports/ui/components/Link.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import LinkList from "./LinkList";
 import PrivateHeader from "./PrivateHeader";
 import AddLink from "./AddLink";
+import Toggler from "./Toggler";
 const Link = () => {
   return (
     <div>
       <PrivateHeader title="Your Links" />
+      <Toggler />
       <LinkList />
       <AddLink />
     </div>
