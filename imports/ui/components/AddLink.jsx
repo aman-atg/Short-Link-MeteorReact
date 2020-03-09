30dee526f91d2f9ec4b2b00c305941f07fb7dfdb
aman-atg <amanansari.a2@gmail.com>
2020-03-09 16:49:37 +0530

    set react-modal

--- imports/ui/components/AddLink.jsx
+++ imports/ui/components/AddLink.jsx
@@ -1,33 +1,59 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Meteor } from "meteor/meteor";
-
+import ReactModal from "react-modal";
 class AddLink extends React.Component {
   state = {
-    url: ""
+    url: "",
+    isOpen: false,
+    err: ""
   };
   onUrlChange = e => {
     const url = e.target.value;
     this.setState({ url });
+  };
+  handleModalClose = () => {
+    this.setState({ isOpen: false, url: "", err: "" });
   };
   render() {
     const onFormSubmit = e => {
       e.preventDefault();
       const { url } = this.state;
       Meteor.call("links.insert", url.trim(), (err, res) => {
-        if (!err) this.setState({ url: "" });
+        if (!err) this.setState({ url: "", err: "" });
+        else this.setState({ err });
       });
     };
 
+    const urlRef = React.createRef();
     return (
-      <form onSubmit={onFormSubmit}>
-        <input
-          type="text"
-          name="url"
-          onChange={this.onUrlChange}
-          value={this.state.url}
-        />
-        <button>Submit</button>
-      </form>
+      <Fragment>
+        {this.state.err ? <p>{this.state.err}</p> : undefined}
+        <button onClick={() => this.setState({ isOpen: true })}>
+          + Add Link
+        </button>
+
+        <ReactModal
+          ariaHideApp={false}
+          isOpen={this.state.isOpen}
+          contentLabel="Add Links"
+          onAfterOpen={() => {
+            urlRef.current.focus();
+          }}
+          onRequestClose={() => this.handleModalClose()}
+        >
+          <form onSubmit={onFormSubmit}>
+            <input
+              ref={urlRef}
+              type="text"
+              name="url"
+              onChange={this.onUrlChange}
+              value={this.state.url}
+            />
+            <button>Submit</button>
+          </form>
+          <button onClick={() => this.handleModalClose()}>Cancel</button>
+        </ReactModal>
+      </Fragment>
     );
   }
 }
