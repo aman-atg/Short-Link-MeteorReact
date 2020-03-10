9b9331f555c5d527a657ae4c2f26db71097a44e3
aman-atg <amanansari.a2@gmail.com>
2020-03-10 21:21:43 +0530

    no link msg + style

--- imports/ui/components/LinkList.jsx
+++ imports/ui/components/LinkList.jsx
@@ -16,11 +16,14 @@ class LinkList extends Component {
     this.linkTracker.stop();
   }
   renderListItems = () => {
-    const links = this.state.links.map(link => {
+    const { links } = this.state;
+    if (links.length === 0)
+      return <p className="item__status-message">No Links Found</p>;
+
+    return links.map(link => {
       const shortUrl = Meteor.absoluteUrl(link._id);
       return <ListItem key={link._id} shortUrl={shortUrl} {...link} />;
     });
-    return links;
   };
   render() {
     return (
